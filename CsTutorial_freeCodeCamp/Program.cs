2d0f70402f7abbfea063113b1fad866602b8030a
Nuno Henriques <nuno.henriques@izertislabs.pt>
2024-09-10 22:37:50 +0100

    update

--- CsTutorial_freeCodeCamp/Program.cs
+++ CsTutorial_freeCodeCamp/Program.cs
@@ -12,6 +12,87 @@ namespace CsTutorial_freeCodeCamp
         {
             Console.WriteLine("Hello World");
             Console.ReadLine();
+
+            // Data Types
+            string phrase = "abc";
+            char singleChar = 'a';  
+            int wholeNumber = 0;
+            float decimalNumber1 = 1.3F;
+            double decimalNumber2 = 2.2;
+            decimal decimalNumber3 = 3.2M;
+            bool boolean = false;
+
+            // Working with strings
+            Console.WriteLine("new\nline");
+            Console.WriteLine(phrase.ToUpper());
+            Console.WriteLine(phrase[0]);
+
+            // Convert
+            int num = Convert.ToInt32("43");
+            double num2 = Convert.ToDouble("43,32");
+
+            // Arrays
+            int[] arrayOfNumbers = { 4, 8, 32 };
+            Console.WriteLine(arrayOfNumbers[0]);
+            arrayOfNumbers[1] = 900;
+
+            string[] arrayOfStrings = { "abc", "def", "123qwe" };
+            string[] arrayOfStrings2 = new string[5];
+            arrayOfStrings2[0] = "qwe";
+            arrayOfStrings2[1] = "asd";
+            Console.WriteLine(arrayOfStrings2[0]);
+
+
+            // Methods
+            simpleMethod("John");
+            Console.WriteLine(cube(5));
+
+            // Switch
+            int dayNum = 1;
+            string dayName;
+            switch (dayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+
+                case 1:
+                    dayName = "Monday";
+                    break;
+
+                default:
+                    dayName = "Invalid";
+                    break;
+            }
+
+
+            // While loop
+            int index = 1;
+            while (index < 5)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+
+            // Do While loop - corre sempre pelo menos uma vez
+            do
+            {
+                Console.WriteLine(index);
+                index++;
+            } while (index <= 0);
+
+            Console.ReadLine();
+        }
+
+        static void simpleMethod(string name) // void - sem retorno
+        {
+            Console.WriteLine("Hello " + name);
+        }
+
+        static int cube(int num) // retorna int
+        {
+            int result = num * num * num;
+            return result;
         }
     }
 }
